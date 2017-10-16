11d2328fa33d7442ee228711c21001300e116f75
dudleybaumer <funkydollahbill+github@gmail.com>
2017-10-16 15:16:16 -0700

    lists ex1

---
+++ pynet_lists_ex1.py
@@ -0,0 +1,13 @@
+my_list = ['ashley', 'ben', 'buck', 'bstout', 'krishelle']
+my_list.append('tony')
+my_list.append('naveen')
+print (my_list)
+
+my_list.pop(0)
+print (my_list)
+
+l=len(my_list)
+print (l)
+
+my_list.sort()
+print (my_list)
